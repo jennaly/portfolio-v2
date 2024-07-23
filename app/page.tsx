eb07d0187141912a34819ede9ff7f82dd78891ed
jennaly <jenna@wicot.io>
2024-07-23 16:33:58 -0700

    refactor: modularize components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-import Projects from "./components/Projects";
+import About from "./components/Sections/About";
+import Projects from "./components/Sections/Projects";
 
 export default function Home() {
   return (
-    <main className="max-w-screen-xl mx-auto p-20 border border-white">
-      <section className="flex flex-col gap-10">
-        <h2 className="text-4xl">View My Work</h2>
-        <Projects />
-      </section>
+    <main className="max-w-screen-xl mx-auto p-20 flex flex-col gap-20 border border-white">
+      <Projects />
+      <About />
     </main>
   );
 }
