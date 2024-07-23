6cd1c696305049368745faff9eadfcc89720970e
jennaly <jenna@wicot.io>
2024-07-23 16:34:59 -0700

    feat: shared h2 headers

---
+++ app/components/shared/H2.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+type H2Props = {
+  children: React.ReactNode;
+};
+
+const H2 = ({ children }: H2Props) => {
+  return <h2 className="text-4xl">{children}</h2>;
+};
+
+export default H2;
