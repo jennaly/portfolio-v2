f4944be29ac2b469aec77e8efbe2fcb18b73cdab
jennaly <106183040+jennaly@users.noreply.github.com>
2024-08-27 16:33:25 -0700

    refactor(ui): font change

--- app/components/Sections/Hero.tsx
+++ app/components/Sections/Hero.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import EmailButton from "../EmailButton";
-import { DM_Sans } from "next/font/google";
+import { DM_Sans, Signika_Negative } from "next/font/google";
 
 const DMSans = DM_Sans({ subsets: ["latin"] });
+const SigNegative = Signika_Negative({ subsets: ["latin"] });
 
 const Hero = () => {
   return (
     <div className="flex flex-col mx-auto items-center text-center justify-between gap-5">
-      <div className={`${DMSans.className} text-5xl lg:text-6xl xl:text-7xl`}>
-        <h1>Jenna Ly</h1>
-        <span className="text-accent">Web Developer</span>
+      <div className="text-5xl lg:text-6xl xl:text-7xl">
+        <h1 className={`${DMSans.className}`}>Jenna Ly</h1>
+        <span
+          className={`${SigNegative.className} text-accent text-4xl uppercase font-semibold`}
+        >
+          full-stack development
+        </span>
       </div>
 
       <p className="text-xl tracking-wide max-w-[600px]">
