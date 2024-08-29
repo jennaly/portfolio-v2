5fe6ce4022e0d11ffdfa4e8a0d176046532dbb38
jennaly <106183040+jennaly@users.noreply.github.com>
2024-08-29 14:43:35 -0700

    refactor(ui): increase padding at bottom of page

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} relative max-w-screen-xl mx-auto`}>
+      <body
+        className={`${inter.className} relative max-w-screen-xl mx-auto pb-10`}
+      >
         <Nav />
         {children}
         <Footer />
